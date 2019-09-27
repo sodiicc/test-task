8add6dfd9fcb2c06abf00adaef4c26bc8c4a994d
Vasiliy Tsyvinskyi <sodiicc@gmail.com>
2019-09-27 17:52:12 +0300

    add build

---
+++ dots/build/precache-manifest.963ce7bb5f7f265e3d5b6aab0bdc8e42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553317d3b8607c9ff7167344ae045f45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "327c094fb53865e1a44c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "fb66a768edf6aebe2019",
+    "url": "/static/js/2.1060b2c3.chunk.js"
+  },
+  {
+    "revision": "327c094fb53865e1a44c",
+    "url": "/static/js/main.e96d262e.chunk.js"
+  },
+  {
+    "revision": "3316cab8313742e870bd",
+    "url": "/static/js/runtime~main.3225056a.js"
+  }
+]);
